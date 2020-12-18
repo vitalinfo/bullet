8bdf9ea337d735307effeb2b93f1e8b5aa79f60e
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2020-12-18 07:40:25 +0000

    Rails 6.2

--- lib/bullet/dependency.rb
+++ lib/bullet/dependency.rb
@@ -29,6 +29,8 @@ module Bullet
             'active_record60'
           elsif active_record61?
             'active_record61'
+          elsif active_record62?
+            'active_record62'
           else
             raise "Bullet does not support active_record #{::ActiveRecord::VERSION::STRING} yet"
           end
@@ -94,6 +96,10 @@ module Bullet
 
     def active_record61?
       active_record6? && ::ActiveRecord::VERSION::MINOR == 1
+    end
+
+    def active_record62?
+      active_record6? && ::ActiveRecord::VERSION::MINOR == 2
     end
 
     def mongoid4x?
